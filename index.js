6406ecab0bca995b08754afabbfed24a15e52792
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-04 17:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const app = "I don't do much."
+kittens = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(Name) {
+kittens.push(Name)
+}
+function destructivelyPrependKitten(Name) { kittens.unshift(Name)
+}
+function destructivelyRemoveLastKitten() { kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+function appendKitten(Name) {
+  kittens.append(Name)
+}
+function prependKitten(Name) {
+  kittens.prepend(Name)
+}
